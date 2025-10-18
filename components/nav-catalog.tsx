8ef3fd0a6704ace981216735466d6d6634fe58b5
Shayne Meyer <shayne.meyer@gmail.com>
2025-10-18 06:42:30 -0700

    fixed build issues

--- components/nav-catalog.tsx
+++ components/nav-catalog.tsx
@@ -1,28 +1,15 @@
 "use client";
 
 import {
-  Folder,
-  MoreHorizontal,
-  Share,
-  Trash2,
   type LucideIcon,
 } from "lucide-react";
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
   SidebarGroup,
   SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from "@/components/ui/sidebar";
 
 export function NavCatalog({
@@ -34,7 +21,6 @@ export function NavCatalog({
     icon: LucideIcon;
   }[];
 }) {
-  const { isMobile } = useSidebar();
 
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
